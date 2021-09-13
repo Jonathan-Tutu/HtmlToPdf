ac535599f04c29d5f4d8e4a2bc69e744250cc59e
Jonathan-Tutu <82053377+Jonathan-Tutu@users.noreply.github.com>
2021-09-13 09:14:15 +0200

    Update Test.py
    Devrait normalement permettre de skip si les images ne sont pas présente dans le dossier

--- Test.py
+++ Test.py
@@ -1,4 +1,4 @@
 import pdfkit 
 
-kitoptions = {"enable-local-file-access": None, "encoding":"UTF-8"}
+kitoptions = {"enable-local-file-access": None, "encoding":"UTF-8", "load-media-error-handling": "Skip"} #Dernier tuple du dictionnaire permet normalement de skip s'il arrive pas a lire le media 
 pdfkit.from_file("C:/Users/Jonathan/Desktop/HTML_THOMAS/test.html", "Out.pdf", options=kitoptions)
